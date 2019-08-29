642c736973c53b02193e438e503489cc87778d6f
ngshya <ngshya@gmail.com>
2019-08-29 22:19:55 +0200

    Created the get started file.

---
+++ Get-started.py
@@ -0,0 +1,10 @@
+#%% [markdown]
+# # Get started with TensorFlow 2.0 for beginners
+
+
+#%%
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+print(tf.__version__)
+
+#%%
